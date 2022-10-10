2b5f0f363b4c19368579c858b6c332ac05b42702
Symphon-y <travisredden@outlook.com>
2022-10-10 00:30:57 -0500

    updated randomMovie command, styled embed

--- commands/randomMovie.js
+++ commands/randomMovie.js
@@ -24,10 +24,64 @@ module.exports = {
         const randomMovie = Math.floor(Math.random() * library.length);
         // console.log(library);
         console.log(library[randomMovie]);
-        interaction.reply(library[randomMovie].title);
+        const options = {
+          method: 'GET',
+          url: 'https://api.themoviedb.org/3/search/movie?',
+          params: {
+            api_key: process.env.TMDB_API_KEY,
+            query: library[randomMovie].title,
+          },
+        };
+
+        axios(options)
+          .then((data) => {
+            console.log(data.data.results[0]);
+            const posterPath = data.data.results[0].poster_path;
+            const movieEmbed = new EmbedBuilder()
+              .setColor('#b90303')
+              .setTitle(library[randomMovie].title)
+              .setURL(
+                `https://www.themoviedb.org/movie/${data.data.results[0].id}`
+              )
+              // .setAuthor({
+              //   name: 'Some name',
+              //   iconURL: 'https://i.imgur.com/AfFp7pu.png',
+              //   url: 'https://discord.js.org',
+              // })
+              .setDescription(library[randomMovie].summary)
+              // .setThumbnail(
+              //   `${process.env.THUMB_URL}${library[randomMovie].thumb}`
+              // )
+              .addFields(
+                // { name: 'Regular field title', value: 'Some value here' },
+                // { name: '\u200B', value: '\u200B' },
+                {
+                  name: 'Content Rating',
+                  value: `${library[randomMovie].contentRating}`,
+                  inline: true,
+                },
+                {
+                  name: 'Critic Rating',
+                  value: `${library[randomMovie].rating}`,
+                  inline: true,
+                }
+              )
+              .addFields({
+                name: 'Audience Rating',
+                value: `${library[randomMovie].audienceRating}`,
+                inline: true,
+              })
+              .setImage(`https://image.tmdb.org/t/p/original${posterPath}`)
+              .setTimestamp()
+              .setFooter({
+                text: `Directed By: ${library[randomMovie].Director[0].tag}`,
+              });
+            interaction.reply({ embeds: [movieEmbed] });
+          })
+          .catch((err) => {
+            console.error('Could not connect to server', err);
+          });
       })
-      .catch((err) => {
-        console.error('Could not connect to server', err);
-      });
+      .catch((error) => console.log(error));
   },
 };
